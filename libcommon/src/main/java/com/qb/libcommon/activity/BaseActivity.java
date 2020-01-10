6327a9ffed1b6d039040109f7ddb4272108fb794
wuqiubin <wuqiubin@mfexcel.com>
2020-01-10 19:10:34 +0800

    common库接入

---
+++ libcommon/src/main/java/com/qb/libcommon/activity/BaseActivity.java
@@ -0,0 +1,46 @@
+package com.qb.libcommon.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+/**
+ * 基础Activity类
+ * */
+public abstract class BaseActivity extends FragmentActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        initView();
+        initData();
+    }
+
+    /**初始化布局*/
+    protected abstract int getLayoutId();
+    /**实例化布局*/
+    protected abstract void initView();
+    /**加载数据*/
+    protected abstract void initData();
+
+    /**
+     * 基础显示加载进度条
+     * */
+    protected void showLoading(){}
+    /**
+     * 显示加载进度条
+     * @param layoutId 进度条自定义样式
+     * */
+    protected void showLoading(int layoutId){}
+
+    /**隐藏进度条*/
+    protected void dismissLoading(){}
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ libcommon/src/main/java/com/qb/libcommon/activity/BaseToolBarActivity.java
@@ -0,0 +1,20 @@
+package com.qb.libcommon.activity;
+
+import com.qb.libcommon.R;
+
+public class BaseToolBarActivity extends BaseActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_base_toolbar;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
