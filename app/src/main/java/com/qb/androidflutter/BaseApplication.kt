eb51c368fd9ba0230714420c8b2bf83d7536865d
wuqiubin <wuqiubin@mfexcel.com>
2020-01-10 16:41:30 +0800

    Android项目中嵌入Flutter Flutter作为module存在

---
+++ app/src/main/java/com/qb/androidflutter/BaseApplication.kt
@@ -0,0 +1,32 @@
+package com.qb.androidflutter
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class BaseApplication : Application() {
+    private lateinit var flutterEngine : FlutterEngine
+    companion object{
+        var engineId:String = "qb_android_engine_id"
+    }
+    override fun onCreate() {
+        super.onCreate()
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put(engineId, flutterEngine)
+    }
+
+
+
+}
